58a0df063785c1352960c2f9d514e42d09da2fb1
Aurorassss <105535241+Aurorassss@users.noreply.github.com>
2022-05-14 14:23:59 +0800

    Update getList.js

--- 2/getList.js
+++ 2/getList.js
@@ -15,10 +15,10 @@ title = "外出实习 八点半左右出校门坐公交去大学城北地铁站�
 
 type = "实习";
 
+state = 3;
+
 start = "2022-05-09 8:48";
 end = "2022-05-15 23:48";
-
-state = 3;
 
 Date.prototype.format=function(fmt){var o={"M+":this.getMonth()+1,"d+":this.getDate(),"h+":this.getHours(),"m+":this.getMinutes(),"s+":this.getSeconds(),"q+":Math.floor((this.getMonth()+3)/3),"S":this.getMilliseconds()};if(/(y+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length))};for(var k in o){if(new RegExp("("+k+")").test(fmt)){fmt=fmt.replace(RegExp.$1,(RegExp.$1.length==1)?(o[k]):(("00"+o[k]).substr((""+o[k]).length)))}};return fmt};
 
