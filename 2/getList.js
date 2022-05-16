5c9d7bc0c7f8839d079801892523d4ab0af063e2
Aurorassss <105535241+Aurorassss@users.noreply.github.com>
2022-05-16 15:19:58 +0800

    Update getList.js

--- 2/getList.js
+++ 2/getList.js
@@ -17,8 +17,8 @@ type = "实习";
 
 state = 3;
 
-start = "2022-05-09 8:48";
-end = "2022-05-15 23:48";
+start = "2022-05-16 8:48";
+end = "2022-05-22 23:48";
 
 Date.prototype.format=function(fmt){var o={"M+":this.getMonth()+1,"d+":this.getDate(),"h+":this.getHours(),"m+":this.getMinutes(),"s+":this.getSeconds(),"q+":Math.floor((this.getMonth()+3)/3),"S":this.getMilliseconds()};if(/(y+)/.test(fmt)){fmt=fmt.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length))};for(var k in o){if(new RegExp("("+k+")").test(fmt)){fmt=fmt.replace(RegExp.$1,(RegExp.$1.length==1)?(o[k]):(("00"+o[k]).substr((""+o[k]).length)))}};return fmt};
 
